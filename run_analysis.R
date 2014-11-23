2209e5776bcd0c4263bd0695553c989c48c10d88
Peter Van Hoof <peter-van-hoof@skynet.be>
2014-11-23 15:03:07 +0100

    code cleanup and adding comments

--- run_analysis.R
+++ run_analysis.R
@@ -17,41 +17,41 @@ library(reshape2)
 features <- read.table("./data/UCI HAR Dataset/features.txt")
 activityLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
 
-# test data
+# Merge test data into dfTest
 subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 xtest <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
 ytest <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
-#dfTest <- cbind(subject_test,ytest$V1,(rep("test", nrow(xtest))), xtest)
 dfTest <- cbind(subject_test,ytest$V1, xtest)
 colnames(dfTest) <- features$V2
-#names(dfTest)[1:3] <- c("Activity","Subject","OriginalDataSet")
+names(dfTest)[1:2] <- c("Subject", "Activity")
 
+# Merge training data into dfTrain
 subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 xtrain <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
 ytrain <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
-# dfTrain <- cbind(subject_train,ytrain$V1,(rep("train", nrow(xtrain))), xtrain)
 dfTrain <- cbind(subject_train,ytrain$V1, xtrain)
 colnames(dfTrain) <- features$V2
-#names(dfTrain)[1:3] <- c("Activity","Subject","OriginalDataSet")
+names(dfTrain)[1:2] <- c("Subject", "Activity")
 
+# Merge "test data" (dfTest) and "training data" (dfTrain) into one dataframe "df"
 df <- rbind(dfTest, dfTrain)
-#names(df)[1:3] <- c("Activity","Subject","OriginalDataSet")
-names(df)[1:2] <- c("Activity","Subject")
-
-
+df <- tbl_df(df)
 
 # 2. Extracts only the measurements on the mean and standard deviation 
 #    for each measurement. 
-df <- df[,grep("Activity|Subject|OriginalDataSet|mean|std",colnames(df),value=F)]
+df <- df[,grep("Subject|Activity|mean|std",colnames(df),value=F)]
+# remove "meanFreq"
 df <- df[,grep("meanFreq",colnames(df),invert = TRUE)]
 
 # 3. Uses descriptive activity names to name the activities in the data set
-df$Activity <- activityLabels[
-        match(df$Activity, activityLabels$V1) , 2]
+df$Activity <- activityLabels[match(df$Activity, activityLabels$V1) , 2]
 
 # 4. Appropriately labels the data set with descriptive variable names. 
+OriginalColumnNames <- names(df)
 names(df)<-gsub("fBody","FrequencyBody",names(df),ignore.case=T)
 names(df)<-gsub("tBody","TimeBody",names(df),ignore.case=T)
+names(df)<-gsub("tGravity","TimeGravity",names(df),ignore.case=T)
+names(df)<-gsub("BodyBody","Body",names(df),ignore.case=T)
 names(df)<-gsub("mean","Mean",names(df),ignore.case=T)
 names(df)<-gsub("std","StandardDeviation",names(df),ignore.case=T)
 names(df)<-gsub("Mag","Magnitude",names(df),ignore.case=T)
@@ -62,9 +62,12 @@ names(df)<-gsub("-X","Xaxis",names(df),ignore.case=T)
 names(df)<-gsub("-Y","Yaxis",names(df),ignore.case=T)
 names(df)<-gsub("-Z","Zaxis",names(df),ignore.case=T)
 names(df)<-gsub("-","",names(df),ignore.case=T)
+NewColumnNames <- names(df)
+ColumnNamesTransformation = cbind(OriginalColumnNames, NewColumnNames)
 
 # 5. From the data set in step 4, creates a second, independent tidy data set 
 #    with the average of each variable for each activity and each subject.
-melted <- melt(df, id.vars = c("Subject", "Activity"))
-tidyData <- dcast(Subject + Activity ~ variable, data = melted, fun = mean)
-write.table(result, file = "tidydata.txt", sep = "\t", row.names = FALSE, quote=FALSE)
+df <- melt(df, id.vars = c("Subject", "Activity"))
+df <- dcast(Subject + Activity ~ variable, data = df, fun = mean)
+df <- melt(df, id.vars = c("Subject", "Activity"))
+write.table(df, file = "tidydata.txt", sep = "\t", row.names = FALSE, quote=FALSE)
